b6c857dc0888467e4a0dfc5504812b0a2966aca0
ehassah <ehassah@gmail.com>
2019-03-12 19:23:41 -0700

    Added code to extract description

--- scrape.py
+++ scrape.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import urllib.request as request
+import csv
 # website urls
 url = "https://www.allrecipes.com/"
 
@@ -33,6 +34,10 @@ BrowseCategories = driver.find_element_by_xpath("//a[text()='All Categories']")
 BrowseCategories.click()
 
 driver.implicitly_wait(3)
+data=[]
+#creating csv file
+f = csv.writer(open('data.csv', 'w'))
+f.writerow(['ID', 'Name', 'SubmitedBy', 'Ingredients', 'Direction', 'Type', 'Servings', 'Calories', 'PrepTime', 'CookTime', 'ReadyIn', 'ImageName'])
 
 
 # getting all the list of Categories
@@ -41,9 +46,21 @@ sections = len(driver.find_elements_by_xpath(li_path))
 
 print(sections)
 #
+recipe_name = ""
+submittedBy= ""
+ingredientsArray = ""
+directionArray = ""
+typeOfMeal = ""
+serving = ""
+calories = ""
+prepTime = ""
+cookTime = ""
+readyTime = ""
+ImageName = ""
 for s in range(sections-1):
 
     print('*****************************clicked on ' + driver.find_element_by_xpath(li_path+'['+str(s+1)+']//a').text+'**********************************')
+    typeOfMeal = driver.find_element_by_xpath(li_path+'['+str(s+1)+']//a').text
     driver.find_element_by_xpath(li_path+'['+str(s+1)+']//a').click()
 
     driver.implicitly_wait(3)
@@ -71,6 +88,8 @@ for s in range(sections-1):
             alt_recipe_name_xpath = "//div[@class='intro article-info']/h1"
             # recipe_name = driver.find_element_by_xpath(recipe_name_xpath)
             # alt_recipe_name = driver.find_element_by_xpath(alt_recipe_name_xpath)
+            ingredientsArray = ""
+            directionArray = ""
             if len(driver.find_elements_by_xpath(recipe_name_xpath)) > 0:
                 recipe_name = driver.find_element_by_xpath(recipe_name_xpath)
                 print('################'+recipe_name.text+'####################')
@@ -82,12 +101,61 @@ for s in range(sections-1):
                 folder = 'C:/Users/ehass/Documents/Dev/Web Scraping/Images/'
                 request.urlretrieve(icon.img['src'], folder + (icon.img['alt']).replace('"', '').replace('Photo of ', '') + '.jpeg')
 
+                if len(driver.find_elements_by_xpath("//div[@class='hero-photo__wrap']//img")) > 1:
+                    ImageName = driver.find_element_by_xpath("//div[@class='hero-photo__wrap']//img[2]").get_attribute("alt")
+                else:
+                    ImageName = driver.find_element_by_xpath("//div[@class='hero-photo__wrap']//img").get_attribute("alt")
+
+                # getting servings
+                if len(driver.find_elements_by_xpath("//div[@class='submitter']//span[@class='submitter__name']")) > 0:
+                    submittedBy = driver.find_element_by_xpath("//div[@class='submitter']//span[@class='submitter__name']").text
+                    print("Submitted by : " + str(submittedBy))
+
+                # getting servings
+                if len(driver.find_elements_by_xpath("//span[@class ='servings-count']")) > 0:
+                    serving = driver.find_element_by_xpath("//span[@class ='servings-count']/span[1]").text
+                    print("Servings : "+ str(serving)+" servings")
+
+                # getting calories
+                if len(driver.find_elements_by_xpath("//span[@class ='calorie-count']")) > 0:
+                    calories = driver.find_element_by_xpath("//span[@class ='calorie-count']/span[1]").text
+                    print("Calories : " + str(calories) + " cals")
+
+
+                # getting the ingredients
                 ingredients_xpath = "//li[@class ='checkList__line']//label//span"
                 ingredients = len(driver.find_elements_by_xpath(ingredients_xpath))
                 print("Ingredients are - ")
+
                 for j in range(ingredients):
                     ingredient_name = driver.find_elements_by_xpath(ingredients_xpath)[j].text
-                    print(ingredient_name)
+                    ingredientsArray = ingredientsArray +"$$$"+ ingredient_name
+                print(ingredientsArray)
+
+                # getting prep time
+                if len(driver.find_elements_by_xpath("//time[@itemprop='prepTime']/span")) > 0:
+                    prepTime = driver.find_element_by_xpath("//time[@itemprop='prepTime']/span").text
+                    print("Prep time is : "+ str(prepTime))
+
+                # getting cook time
+                if len(driver.find_elements_by_xpath("//time[@itemprop='cookTime']/span")) > 0:
+                    cookTime = driver.find_element_by_xpath("//time[@itemprop='cookTime']/span").text
+                    print("Cook time is : "+ str(cookTime))
+
+                # getting ready in time
+                if len(driver.find_elements_by_xpath("//time[@itemprop='totalTime']/span")) > 0:
+                    readyTime = driver.find_element_by_xpath("//time[@itemprop='totalTime']/span").text
+                    print("Ready in time is : "+ str(readyTime))
+
+                # getting the directions
+                direction_xpath = "//div[@class='directions--section__steps ng-scope']//ol//li[@class='step']"
+
+                directions = len(driver.find_elements_by_xpath(direction_xpath))
+                for d in range(directions-1):
+                    direction = driver.find_element_by_xpath(direction_xpath + '[' + str(d + 1) + ']').text
+                    directionArray = directionArray + "$$$" + direction
+                print(directionArray)
+
                 driver.execute_script("window.history.go(-1)")
                 driver.implicitly_wait(5)
 
@@ -98,13 +166,23 @@ for s in range(sections-1):
                 ingredients_xpath = "//ul[@class='ingredients-section']//li//span[@class='ingredients-item-name']"
                 ingredients = len(driver.find_elements_by_xpath(ingredients_xpath))
                 print("Ingredients are - ")
+
                 for j in range(ingredients):
                     ingredient_name = driver.find_elements_by_xpath(ingredients_xpath)[j].text.strip()
-                    print(ingredient_name)
+                    ingredientsArray = ingredientsArray + "$$$" + ingredient_name
+                print(ingredientsArray)
 
+
+                tempData = [i, recipe_name, submittedBy, ingredientsArray, directionArray, typeOfMeal, serving, calories, prepTime, cookTime, readyTime, ImageName]
+                data.append(tempData)
                 driver.execute_script("window.history.go(-1)")
                 driver.implicitly_wait(5)
 
-
+    with open('data.csv', 'wb') as file:
+        writer = csv.writer(file)
+        for row in data:
+            writer.writerow(row)
     driver.execute_script("window.history.go(-1)")
     driver.implicitly_wait(5)
+
+
